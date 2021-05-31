32c3e36bdf115203bbec2bec12dd9f74e85470f6
RIZZO Leonard <Leonard.Rizzo@bordeaux-inp.fr>
2021-05-31 18:08:57 +0200

    added action on pull requests (no effect visible)

--- breakbot-test1/index.js
+++ breakbot-test1/index.js
@@ -7,11 +7,24 @@ module.exports = (app) => {
   app.log.info("Yay, the app was loaded!");
 
   app.on("issues.opened", async (context) => {
+    app.log.info("An issue was opened !");
     const issueComment = context.issue({
       body: "Thanks for opening this issue!",
     });
     return context.octokit.issues.createComment(issueComment);
   });
+
+  app.on("pull_request.opened", async (context) => { //just a skeleton waiting for tests, not sure it works
+    app.log.info("A pull request was opened !");
+    const prComment = context.pullRequest({
+      body: "Thanks for opening this pull request!",
+    });
+    return context.octokit.pullRequest.createComment(prComment);
+  });
+
+  app.on("pull_request.assigned", async (context) => {
+    app.log.info("Someone was assigned !");
+  })
 
   // For more information on building apps:
   // https://probot.github.io/docs/
