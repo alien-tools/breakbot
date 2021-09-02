38e67e3d3f727297ca3fff405d577f70a864712c
Thomas Degueule <thomas.degueule@labri.fr>
2021-09-02 17:59:43 +0200

    Simply giving up on template strings...

--- src/formatJson.ts
+++ src/formatJson.ts
@@ -1,3 +1,5 @@
+import { stripIndent } from 'common-tags'
+
 export function parseJson(myJson: any, maxBCs: number, maxClients: number, maxDetections: number) {
     if (myJson.report.error != null)
         return ([myJson.report.error, null, null])
@@ -10,35 +12,49 @@ export function parseJson(myJson: any, maxBCs: number, maxClients: number, maxDe
 
     const title = "Break-bot report"
 
-    const summary =
-        `This pull request introduces **${bcs.length} breaking changes**, causing **${detections.length} detections** in client code.
+    var summary = stripIndent`
+        This pull request introduces **${bcs.length} breaking changes**, causing **${detections.length} detections** in client code.
         **${brokenClients.length} of ${clients.length} clients are impacted** by the changes (${brokenClients.length / clients.length * 100}%).
 
         #### Breaking changes
-        
         Declaration | Kind | Impacted clients
         ----------- | ---- | ----------------
-        ${bcs.slice(0, maxBCs).map((bc: any) =>
-            `[\`${bc.declaration}\`](${bc.url}) | [\`${bc.change}\`]() | <WIP>`
-        ).join("\n")}
+    `
 
+    
+    bcs.slice(0, maxBCs).forEach((bc: any) => {
+        summary += "\n"
+        summary += `[\`${bc.declaration}\`](${bc.url}) | [\`${bc.change}\`]() | WIP`
+    })
+
+    summary += "\n\n"
+    summary += stripIndent`
         #### Impact on clients
         Client | Status | Detections
         ------ | ------ | ----------
-        ${clients.slice(0, maxClients).map((c : any) =>
-            `[${c.url}](${c.url}) | ${c.detections.length > 0 ? `:x:` : `:heavy_check_mark`} | ${c.detections.length}`
-        ).join("\n")}`
+    `
 
-    const message =
-        `${brokenClients.map((c: any) =>
-            `#### [${c.url}](${c.url})
+    clients.slice(0, maxClients).forEach((c : any) => {
+        summary += "\n"
+        summary += `[${c.url}](${c.url}) | ${c.detections.length > 0 ? `:x:` : `:heavy_check_mark`} | ${c.detections.length}`
+    })
+
+    var message = ""
+    brokenClients.forEach((c: any) => {
+        message += stripIndent`
+            #### [${c.url}](${c.url})
 
             Location | Breaking declaration | Kind | Use Type  
             -------- | -------------------- | ---- | --------
-            ${c.detections.slice(0, maxDetections).map((d: any) =>
-                `[\`${d.elem}\`](${d.url}) | \`${d.src}\` | <WIP> | \`${d.apiUse}\``
-            ).join("\n")}`
-        ).join("\n")}`
+        `
+
+        c.detections.slice(0, maxDetections).forEach((d: any) => {
+            message += "\n"
+            message += `[\`${d.elem}\`](${d.url}) | \`${d.src}\` | WIP | \`${d.apiUse}\``
+        })
+
+        message += "\n"
+    })
 
     return ([title, summary, message])
 }
