0e047a680286abab58ff019169055a042e866122
Thomas Degueule <thomas.degueule@labri.fr>
2022-10-31 15:10:17 +0100

    Report: hide client errors

--- src/report.ts
+++ src/report.ts
@@ -34,13 +34,13 @@ export default function writeReport(
 
   const { report } = myJson;
 
-  const bcs = report.reports.flatMap((pkg : any) => pkg.delta ? pkg.delta.breakingChanges : []);
+  const bcs = report.reports.flatMap((pkg : any) => (pkg.delta ? pkg.delta.breakingChanges : []));
   const allClientUrls = Array.from(new Set(
     report.reports.flatMap((pkg: any) => pkg.clientReports).flatMap((c: any) => c.url),
   ));
   const allClients = allClientUrls.map((url: any) => getClientReport(report, url));
   const clients = allClients.filter((c: any) => !c.error);
-  const clientsError = allClients.filter((c: any) => c.error);
+  // const clientsError = allClients.filter((c: any) => c.error);
   const brokenClients = clients.filter((c: any) => c.brokenUses.length > 0);
   const brokenUses = brokenClients.flatMap((c: any) => c.brokenUses);
   const percentBroken = clients.length > 0
@@ -50,9 +50,10 @@ export default function writeReport(
   const summary = stripIndent`
         This pull request introduces **${bcs.length} breaking changes**, causing **${brokenUses.length} broken uses** in client code.
         **${brokenClients.length} of ${clients.length} analyzed clients are impacted** by the changes (${percentBroken}%).
-        ${clientsError.length > 0 ? `Maracas encountered an error when attempting to process the following clients: ${allClients.map((c: any) => `[${c.url}](https://github.com/${c.url}) (*${c.error}*)`)
-    .join(', ')}.` : ''}
     `;
+
+  // ${clientsError.length > 0 ? `Maracas encountered an error when attempting to process the following clients: ${allClients.map((c: any) => `[${c.url}](https://github.com/${c.url}) (*${c.error}*)`)
+  // .join(', ')}.` : ''}
 
   let reportMessage = stripIndent`
         ### Breaking changes
--- test/fixtures/reports/client-errors-summary.md
+++ test/fixtures/reports/client-errors-summary.md
@@ -1,3 +1,2 @@
 This pull request introduces **2 breaking changes**, causing **0 broken uses** in client code.
 **0 of 0 analyzed clients are impacted** by the changes (0%).
-Maracas encountered an error when attempting to process the following clients: [alien-tools/client-fixture-b](https://github.com/alien-tools/client-fixture-b) (*Unlucky*), [alien-tools/client-fixture-a](https://github.com/alien-tools/client-fixture-a) (*Unlucky 2*).
